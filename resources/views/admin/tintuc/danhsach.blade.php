c9a381d7f0818660bad13f220c3bec2cb86ae084
nguyenquoccuong2110 <38267992+nguyenquoccuong2110@users.noreply.github.com>
2020-03-05 15:34:14 +0700

    Add files via upload

--- resources/views/admin/tintuc/danhsach.blade.php
+++ resources/views/admin/tintuc/danhsach.blade.php
@@ -11,6 +11,13 @@
                         </h1>
                     </div>
                     <!-- /.col-lg-12 -->
+                    @if(session('thongbao'))
+                        <div class="alret alert-success">
+                            {{session('thongbao')}}
+                            
+                        </div>
+
+                        @endif 
                     <table class="table table-striped table-bordered table-hover" id="dataTables-example">
                         <thead>
                             <tr align="center">
--- resources/views/admin/tintuc/them.blade.php
+++ resources/views/admin/tintuc/them.blade.php
@@ -12,7 +12,25 @@
                     </div>
                     <!-- /.col-lg-12 -->
                     <div class="col-lg-7" style="padding-bottom:120px">
-                        <form action="" method="POST">
+                         @if(count($errors)>0)
+                        <div class="alert alert-danger">
+                            @foreach($errors ->all() as $err)
+                                {{$err}} <br>
+
+                            @endforeach
+
+                        </div>
+                        @endif
+
+                        @if(session('thongbao'))
+                        <div class="alret alert-success">
+                            {{session('thongbao')}}
+                            
+                        </div>
+
+                        @endif 
+                        <form action="admin/tintuc/them" method="POST" enctype="multipart/form-data">
+                            <input type="hidden" name="_token" value="{{csrf_token()}}"/>
                             <div class="form-group">
                                 <label>Thể Loại</label>
                                 <select class="form-control" name="TheLoai" id="TheLoai">
@@ -30,32 +48,36 @@
                                 </select>
                             </div>
                             <div class="form-group">
-                                <label>Category Name</label>
-                                <input class="form-control" name="txtCateName" placeholder="Please Enter Category Name" />
+                                <label>Tiêu Đề</label>
+                                <input class="form-control" name="TieuDe" placeholder="Nhập tiêu đề" />
+                            </div>
+                           
+                            <div class="form-group">
+                                <label>Tóm tắt</label>
+                                <textarea name="TomTat" id="demo"class="form-control ckeditor" rows="3"></textarea>
+                            </div>
+
+                            <div class="form-group">
+                                <label>Nội dung</label>
+                                <textarea name="NoiDung" id="demo"class="form-control ckeditor" rows="5"></textarea>
+                            </div>
+
+                            <div class="form-group">
+                                <label>Hình ảnh</label>
+                                <input type="file" name="Hinh" class="form-control" />
+                                
                             </div>
                             <div class="form-group">
-                                <label>Category Order</label>
-                                <input class="form-control" name="txtOrder" placeholder="Please Enter Category Order" />
-                            </div>
-                            <div class="form-group">
-                                <label>Category Keywords</label>
-                                <input class="form-control" name="txtOrder" placeholder="Please Enter Category Keywords" />
-                            </div>
-                            <div class="form-group">
-                                <label>Category Description</label>
-                                <textarea class="form-control" rows="3"></textarea>
-                            </div>
-                            <div class="form-group">
-                                <label>Category Status</label>
+                                <label>Nổi bật</label>
                                 <label class="radio-inline">
-                                    <input name="rdoStatus" value="1" checked="" type="radio">Visible
+                                    <input name="NoiBat" value="0" checked="" type="radio">Không
                                 </label>
                                 <label class="radio-inline">
-                                    <input name="rdoStatus" value="2" type="radio">Invisible
+                                    <input name="NoiBat" value="1" type="radio">Có
                                 </label>
                             </div>
-                            <button type="submit" class="btn btn-default">Category Add</button>
-                            <button type="reset" class="btn btn-default">Reset</button>
+                            <button type="submit" class="btn btn-default">Thêm</button>
+                            <button type="reset" class="btn btn-default">Làm mới</button>
                         <form>
                     </div>
                 </div>
