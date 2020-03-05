35ca516a7cfd26fbd847272a2ce3a1ac6f4fde99
nguyenquoccuong2110 <38267992+nguyenquoccuong2110@users.noreply.github.com>
2020-03-05 15:31:44 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -24,46 +24,65 @@ Route::get('admin/logout','UserController@getDangXuatAdmin');
 Route::group(['prefix' =>'admin','middleware'=>'adminLogin'], function(){
 	Route::group(['prefix' =>'theloai'], function(){	
 		//admin/theloai/danhsach
-		Route::get('danhsach','TheLoaiController@getDanhSach');
+		Route::get('danhsach','TheloaiController@getDanhSach');
 
-		Route::get('sua/{id}','TheLoaiController@getSua');
+		Route::get('sua/{id}','TheloaiController@getSua');
 
-		Route::post('sua/{id}','TheLoaiController@postSua');
+		Route::post('sua/{id}','TheloaiController@postSua');
 
-		Route::get('them','TheLoaiController@getThem');
+		Route::get('them','TheloaiController@getThem');
 
-		Route::post('them','TheLoaiController@postThem');
+		Route::post('them','TheloaiController@postThem');
 
-		Route::get('xoa/{id}','TheLoaiController@getXoa');
+		Route::get('xoa/{id}','TheloaiController@getXoa');
 
 	});
 
 	Route::group(['prefix' =>'loaitin'], function(){	
 		//admin/loai/them
-		Route::get('danhsach','LoaiTinController@getDanhSach');
+		Route::get('danhsach','LoaitinController@getDanhSach');
 
-		Route::get('sua/{id}','LoaiTinController@getSua');
+		Route::get('sua/{id}','LoaitinController@getSua');
 
-		Route::post('sua/{id}','LoaiTinController@postSua');
+		Route::post('sua/{id}','LoaitinController@postSua');
 
-		Route::get('them','LoaiTinController@getThem');
+		Route::get('them','LoaitinController@getThem');
 
-		Route::post('them','LoaiTinController@postThem');
+		Route::post('them','LoaitinController@postThem');
 
-		Route::get('xoa/{id}','LoaiTinController@getXoa');
+		Route::get('xoa/{id}','LoaitinController@getXoa');
 
 	});
 
 	Route::group(['prefix' =>'tintuc'], function(){	
 		//admin/theloai/them
-		Route::get('danhsach','TinTucController@getDanhSach');
+		Route::get('danhsach','TintucController@getDanhSach');
 
-		Route::get('sua','TinTucController@getSua');
+		Route::get('sua/{id}','TintucController@getSua');
+		Route::post('sua/{id}','TintucController@postSua');
 
-		Route::get('them','TinTucController@getThem');
-		Route::post('them','TinTucController@postThem');
+		Route::get('them','TintucController@getThem');
+		Route::post('them','TintucController@postThem');
+
+		Route::get('xoa/{id}','TintucController@getXoa');
 
 	});
+
+	Route::group(['prefix' =>'slide'], function(){	
+		//admin/theloai/them
+		Route::get('danhsach','SlideController@getDanhSach');
+
+		Route::get('sua/{id}','SlideController@getSua');
+		Route::post('sua/{id}','SlideController@postSua');
+
+		Route::get('them','SlideController@getThem');
+		Route::post('them','SlideController@postThem');
+
+		Route::get('xoa/{id}','SlideController@getXoa');
+
+	});
+
+
 	Route::group(['prefix' =>'user'], function(){	
 		//admin/loai/them
 		Route::get('danhsach','UserController@getDanhSach');
@@ -75,6 +94,12 @@ Route::group(['prefix' =>'admin','middleware'=>'adminLogin'], function(){
 		Route::post('them','UserController@postThem');
 
 		Route::get('xoa/{id}','UserController@getXoa');
+	});
+
+	Route::group(['prefix'=>'comment'], function(){
+		Route::get('xoa/{id}/{idTinTuc}','CommentController@getXoa');
+
+
 	});
 
 
